3a13c66aca85d9afe7efe2ef3b07eee5baede942
Oscar Deits <odeits@gmail.com>
2012-09-10 23:18:17 -0700

    Json Property Names

--- IronClient/Config/Configuration.cs
+++ IronClient/Config/Configuration.cs
@@ -11,16 +11,29 @@ namespace IronIO.Config
     public class Configuration
     {
         public string Name { get; set; }
+        
+        [JsonProperty("host")]
         public string Host { get; set; }
+        
         [DefaultValue("https")]
+        [JsonProperty("protocol")]
         public string Protocol { get; set; }
+        
         [DefaultValue(443)]
+        [JsonProperty("port")]
         public int Port { get; set; }
+        
         [DefaultValue(1)]
+        [JsonProperty("api_version")]
         public int ApiVersion { get; set; }
+        
+        [JsonProperty("project_id")]
         public string ProjectId { get; set; }
+
+        [JsonProperty("token")]
         public string Token { get; set; }
 
+        [JsonProperty("version")]
         public string ClientVersion { get; set; }
     }
 }
