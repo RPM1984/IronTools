0e588a7661d1cff49234dee52423e039c0ddba14
Oscar Deits <odeits@gmail.com>
2012-10-13 11:17:44 -0700

    Fixes StyleCop warnings

--- src/IronCache/Data/CacheItem.cs
+++ src/IronCache/Data/CacheItem.cs
@@ -11,27 +11,42 @@ namespace IronIO.Data
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Iron.io cache item
+    /// </summary>
+    /// <typeparam name="T">Type of the object cached</typeparam>
     [JsonObject(MemberSerialization.OptOut)]
     internal class CacheItem<T>
     {
         #region Properties
-
+        /// <summary>
+        /// Gets or sets the cache name
+        /// </summary>
         public string Cache
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the check and set identifier
+        /// </summary>
         [JsonProperty("cas")]
         public long CheckAndSet
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the cache key
+        /// </summary>
         public string Key
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the value of the cache item
+        /// </summary>
         public T Value
         {
             get; set;
