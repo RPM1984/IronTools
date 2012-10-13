505598b8657d32325e453ab357d9779499be045e
Oscar Deits <odeits@gmail.com>
2012-10-13 11:21:40 -0700

    Fixes StyleCop warnings

--- src/IronWorker/Data/CodeInfo.cs
+++ src/IronWorker/Data/CodeInfo.cs
@@ -8,53 +8,80 @@ namespace IronIO.Data
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Code information class
+    /// </summary>
     [JsonObject]
     public class CodeInfo
     {
         #region Properties
 
+        /// <summary>
+        /// Gets or sets the code identifier
+        /// </summary>
         [JsonProperty(PropertyName = "id")]
         public string Id
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the latest change value
+        /// </summary>
         [JsonProperty(PropertyName = "latest_change")]
         public int LatestChange
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the most recent checksum
+        /// </summary>
         [JsonProperty(PropertyName = "latest_checksum")]
         public string LatestChecksum
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the latest history identifier
+        /// </summary>
         [JsonProperty(PropertyName = "latest_history_id")]
         public string LatestHistoryId
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the code
+        /// </summary>
         [JsonProperty(PropertyName = "name")]
         public string Name
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the project identifier
+        /// </summary>
         [JsonProperty(PropertyName = "project_id")]
         public string ProjectId
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the revision number
+        /// </summary>
         [JsonProperty(PropertyName = "rev")]
         public int Revision
         {
             get; set;
         }
 
+        /// <summary>
+        /// Gets or sets the runtime of the code
+        /// </summary>
         [JsonProperty(PropertyName = "runtime")]
         public string Runtime
         {
