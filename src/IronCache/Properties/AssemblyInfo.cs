561d8e5221570e695a6d08b12b7d0948ded21d94
Oscar Deits <odeits@gmail.com>
2012-10-12 23:01:04 -0700

    Copyright headers

--- src/IronCache/Properties/AssemblyInfo.cs
+++ src/IronCache/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IronCache")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Oscar Deits")]
 [assembly: AssemblyProduct("IronCache")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
