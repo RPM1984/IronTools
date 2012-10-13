6fd21d5a2822214ad06108d27b60244c376cac5d
Oscar Deits <odeits@gmail.com>
2012-10-13 11:15:33 -0700

    Fixes StyleCop warnings

--- src/IronCache/Data/Cache.cs
+++ src/IronCache/Data/Cache.cs
@@ -11,12 +11,21 @@ namespace IronIO.Data
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// Iron.io Cache
+    /// </summary>
     [JsonObject(MemberSerialization.OptOut)]
     public class Cache
     {
+        /// <summary>
+        /// Gets or sets the project identifier
+        /// </summary>
         [JsonProperty("project_id")]
         public string ProjectId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the cache name
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
     }
