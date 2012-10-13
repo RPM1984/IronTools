30a0221bbd6b35cf582440746963362a86b50b14
Oscar Deits <odeits@gmail.com>
2012-10-13 11:10:42 -0700

    Fixes StyleCop warnings

--- src/IronClient/Config/WorkerConfigFactory.cs
+++ src/IronClient/Config/WorkerConfigFactory.cs
@@ -5,20 +5,37 @@
 //-----------------------------------------------------------------------
 namespace IronIO.Config
 {
+    /// <summary>
+    /// ConfigurationFactory for creating configurations for use with Iron.io Worker
+    /// </summary>
     internal class WorkerConfigFactory : ConfigurationFactory
     {
+        /// <summary>
+        /// ConfigurationFactory to wrap
+        /// </summary>
         private ConfigurationFactory configurationFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerConfigFactory" /> class.
+        /// </summary>
         public WorkerConfigFactory()
             : this(new DefaultConfigurationFactory())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WorkerConfigFactory" /> class.
+        /// </summary>
+        /// <param name="configuarationFactory">ConfigurationFactory to wrap</param>
         public WorkerConfigFactory(ConfigurationFactory configuarationFactory)
         {
             this.configurationFactory = configuarationFactory;
         }
 
+        /// <summary>
+        /// Creates a Configuration for use with IronClient
+        /// </summary>
+        /// <returns>A configuration for use with IronClient</returns>
         public override Configuration GetConfiguartion()
         {
             Configuration config = this.configurationFactory.GetConfiguartion();
