0c2a55b4d0a4efbc8d016a18971bd710025b0b13
Oscar Deits <odeits@gmail.com>
2012-10-13 10:58:08 -0700

    Fixes StyleCop warnings

--- src/IronClient/Config/ArgsConfigFactory.cs
+++ src/IronClient/Config/ArgsConfigFactory.cs
@@ -5,25 +5,92 @@
 //-----------------------------------------------------------------------
 namespace IronIO.Config
 {
+    /// <summary>
+    /// Configuration factory for building Configurations from passed in arguments
+    /// </summary>
     internal class ArgsConfigFactory : ConfigurationFactory
     {
+        /// <summary>
+        /// Configuration to wrap
+        /// </summary>
         private ConfigurationFactory configFactory;
+
+        /// <summary>
+        /// Name of the client
+        /// </summary>
         private string name;
+
+        /// <summary>
+        /// Version of the client
+        /// </summary>
         private string version;
+
+        /// <summary>
+        /// Iron.io product name
+        /// </summary>
         private string product;
+
+        /// <summary>
+        /// Host name
+        /// </summary>
         private string host;
+
+        /// <summary>
+        /// Project identifier available from the HUD
+        /// </summary>
         private string projectId;
+
+        /// <summary>
+        /// Token available from the HUD
+        /// </summary>
         private string token;
+
+        /// <summary>
+        /// Transport protocol
+        /// </summary>
         private string protocol;
+
+        /// <summary>
+        /// Version of the Iron.io API to use
+        /// </summary>
         private int apiVersion;
+
+        /// <summary>
+        /// Port number
+        /// </summary>
         private int port;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArgsConfigFactory" /> class.    
+        /// </summary>
+        /// <param name="name">Name of the client</param>
+        /// <param name="version">Version of the client</param>
+        /// <param name="product">Iron.io product name</param>
+        /// <param name="host">Host name</param>
+        /// <param name="port">Port number</param>
+        /// <param name="projectId">Project identifier available from the HUD</param>
+        /// <param name="token">Token available from the HUD</param>
+        /// <param name="protocol">Transport protocol</param>
+        /// <param name="apiVersion">Iron.io API version</param>
         public ArgsConfigFactory(string name, string version, string product, string host, int port, string projectId, string token, string protocol, int apiVersion)
             : this(new DefaultConfigurationFactory(), name, version, product, host, port, projectId, token, protocol, apiVersion)
         {
             // Empty constructor
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArgsConfigFactory" /> class.    
+        /// </summary>
+        /// <param name="configFactory">Configuration factory to wrap</param>
+        /// <param name="name">Name of the client</param>
+        /// <param name="version">Version of the client</param>
+        /// <param name="product">Iron.io product name</param>
+        /// <param name="host">Host name</param>
+        /// <param name="port">Port number</param>
+        /// <param name="projectId">Project identifier available from the HUD</param>
+        /// <param name="token">Token available from the HUD</param>
+        /// <param name="protocol">Transport protocol</param>
+        /// <param name="apiVersion">Iron.io API version</param>
         public ArgsConfigFactory(ConfigurationFactory configFactory, string name, string version, string product, string host, int port, string projectId, string token, string protocol, int apiVersion)
         {
             // TODO: Complete member initialization
@@ -39,6 +106,10 @@ namespace IronIO.Config
             this.port = port;
         }
 
+        /// <summary>
+        /// Creates a new Configuration from passed in arguments
+        /// </summary>
+        /// <returns>Argument based Configuration</returns>
         public override Configuration GetConfiguartion()
         {
             Configuration baseConfig = this.configFactory.GetConfiguartion();
