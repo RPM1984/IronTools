deeb1276262c5aac1a1052fda3c5edd2d5bf136e
Oscar Deits <odeits@gmail.com>
2012-10-13 11:08:49 -0700

    Fixes StyleCop warnings

--- src/IronClient/Config/MQConfigFactory.cs
+++ src/IronClient/Config/MQConfigFactory.cs
@@ -5,20 +5,37 @@
 //-----------------------------------------------------------------------
 namespace IronIO.Config
 {
+    /// <summary>
+    /// Configuration factory for creating Iron.io Message Queue client configurations
+    /// </summary>
     internal class MQConfigFactory : ConfigurationFactory
     {
+        /// <summary>
+        /// ConfigurationFactory to wrap
+        /// </summary>
         private ConfigurationFactory configurationFactory;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MQConfigFactory" /> class.
+        /// </summary>
         public MQConfigFactory()
             : this(new DefaultConfigurationFactory())
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MQConfigFactory" /> class.
+        /// </summary>
+        /// <param name="configuarationFactory">ConfigurationFactory to wrap</param>
         public MQConfigFactory(ConfigurationFactory configuarationFactory)
         {
             this.configurationFactory = configuarationFactory;
         }
 
+        /// <summary>
+        /// Creates a Configuration for use with IronClient
+        /// </summary>
+        /// <returns>A configuration for use with IronClient</returns>
         public override Configuration GetConfiguartion()
         {
             Configuration config = this.configurationFactory.GetConfiguartion();
